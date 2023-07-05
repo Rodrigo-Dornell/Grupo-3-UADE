87de77d8eeba88275b492016f3d561a8654a2852
Nicolas Soriano <nicolasrs0599@gmail.com>
2023-07-04 21:09:06 -0300

    commit nico

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyledEngineProvider } from '@mui/material/styles';
 import PrimarySearchAppBar from './NavBar';
+import { proveedores } from './components/Proveedores';
 
 function App() {
   return (
---
+++ src/components/Proveedores.js
@@ -0,0 +1,17 @@
+export const proveedores = [
+    {
+        nombre: "Nike",
+        id: 1, 
+        cuit: "123456789"
+    },
+    {
+        nombre: "Puma",
+        id: 2, 
+        cuit: "987654321"
+    },
+    {
+        nombre: "Adidas",
+        id: 3, 
+        cuit: "147258369"
+    }
+]
